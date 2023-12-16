45a218d04bfec677997ea03ff123ecd3f23668b3
Bastian Stark <53190965+bastian-stark@users.noreply.github.com>
2023-12-15 17:18:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+#Secret Bidding Auction App
+
+#Intro
 print('Welcome to the secret bidding auction!')
 bidDict = {}
 
 def newEntry():
+    """Create new user entry"""
     name = input('Enter name: ')
     bidPrice = input('Enter Bid Price: $')
     bidDict[name] = bidPrice
     return bidDict
 
 bidDict = newEntry()
-
 auctionStatus = 0
+#Begin auction
 while auctionStatus == 0:
+    #Add users to auction
     cont = input('Would you like to add another bidder? (y/n) ')
     if cont.lower() == "y":
         bidDict = newEntry()
@@ -19,6 +24,7 @@ while auctionStatus == 0:
     else:
         print('Invalid response.')
 
+#Compare bids
 highest = ["name", "0"]
 for key in bidDict:
     if int(bidDict[key]) > int(highest[1]):
